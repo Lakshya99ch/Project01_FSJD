65cd6512f6e69a18f50d00a0b467f3f5202c2f7f
Lakshya Chaudhary <lakshyachaudhary0098@gmail.com>
2022-09-25 20:52:38 +0530

    First Commmit

---
+++ main.java
@@ -0,0 +1,16 @@
+package Project.lockedme;
+
+
+
+public class main {
+	/*Enter your desired Directory path */
+	public static final String path = "C:/Users/ASUS/Simplilearn/FSDproject01/src/Project/folder01"; 
+	
+	public static void main(String[] args) {
+		menus menu = new menus();
+		menu.introScreen();
+		menu.mainMenu();
+	}
+	
+
+}
